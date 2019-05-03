335b940673351e5ebe8f0457fd2bce7d5cbcca15
Adrian Smijulj <adrian1358@gmail.com>
2019-05-03 08:24:47 +0200

    docs: add fixed-columns storybook example

--- stories/FixedColumnsExample.js
+++ stories/FixedColumnsExample.js
@@ -21,13 +21,13 @@ class SimpleResponsiveExample extends React.Component<Props, State> {
     render() {
         return (
             <Story>
-                <title>Simple responsive example</title>
+                <title>Fixed columns example</title>
                 <description>
-                    Depending on the width of the container, items will be placed in different
-                    number of columns.
+                    We passed <strong>2</strong> for the number of columns. Regardless of the screen size,
+                    number of columns will not change.
                 </description>
                 <example>
-                    <Columned className={styles}>
+                    <Columned className={styles} columns={2}>
                         <img
                             alt="Image 1"
                             src={"https://via.placeholder.com/800x600/001F3F/FFFFFF.png?text=1"}
@@ -48,26 +48,6 @@ class SimpleResponsiveExample extends React.Component<Props, State> {
                             alt="Image 5"
                             src={"https://via.placeholder.com/640x480/3D9970/FFFFFF.png?text=5"}
                         />
-                        <img
-                            alt="Image 6"
-                            src={"https://via.placeholder.com/1000x500/2ECC40/FFFFFF.png?text=6"}
-                        />
-                        <img
-                            alt="Image 7"
-                            src={"https://via.placeholder.com/700x1000/01FF70/FFFFFF.png?text=7"}
-                        />
-                        <img
-                            alt="Image 8"
-                            src={"https://via.placeholder.com/800x600/FFDC00/FFFFFF.png?text=8"}
-                        />
-                        <img
-                            alt="Image 9"
-                            src={"https://via.placeholder.com/200x100/FF851B/FFFFFF.png?text=9"}
-                        />
-                        <img
-                            alt="Image 10"
-                            src={"https://via.placeholder.com/200x100/FF4136/FFFFFF.png?text=10"}
-                        />
                     </Columned>
                 </example>
                 <code>{code()}</code>
@@ -77,7 +57,7 @@ class SimpleResponsiveExample extends React.Component<Props, State> {
 }
 
 const code = () => /* React */ `
-<Columned>
+<Columned columns={2}>
     <img
         alt="Image 1"
         src={"https://via.placeholder.com/800x600/001F3F/FFFFFF.png?text=1"}
@@ -97,26 +77,6 @@ const code = () => /* React */ `
     <img
         alt="Image 5"
         src={"https://via.placeholder.com/640x480/3D9970/FFFFFF.png?text=5"}
-    />
-    <img
-        alt="Image 6"
-        src={"https://via.placeholder.com/1000x500/2ECC40/FFFFFF.png?text=6"}
-    />
-    <img
-        alt="Image 7"
-        src={"https://via.placeholder.com/700x1000/01FF70/FFFFFF.png?text=7"}
-    />
-    <img
-        alt="Image 8"
-        src={"https://via.placeholder.com/800x600/FFDC00/FFFFFF.png?text=8"}
-    />
-    <img
-        alt="Image 9"
-        src={"https://via.placeholder.com/200x100/FF851B/FFFFFF.png?text=9"}
-    />
-    <img
-        alt="Image 10"
-        src={"https://via.placeholder.com/200x100/FF4136/FFFFFF.png?text=10"}
     />
 </Columned>
 `;
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,9 +4,11 @@ import { storiesOf } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
 
 import SimpleResponsiveExample from "./SimpleResponsiveExample";
+import FixedColumnsExample from "./FixedColumnsExample";
 
 const stories = storiesOf("react-columned", module);
 
 stories.addDecorator(withKnobs);
 
 stories.add("Simple responsive example", () => <SimpleResponsiveExample />);
+stories.add("Fixed columns example", () => <FixedColumnsExample />);
